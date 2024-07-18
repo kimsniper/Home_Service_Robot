4621bb100cb8092d9a2c360e760ca4e1849d1d65
kimsniper <kim1991mail@gmail.com>
2024-07-18 12:49:43 +0800

    removed unnecessary files

--- catkin_ws/src/scripts/home_service.sh
+++ catkin_ws/src/scripts/home_service.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-xterm  -e  " source ../../devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  " source ../../devel/setup.bash && roslaunch my_robot world.launch " &
 sleep 15
-xterm  -e  " source ../../devel/setup.bash && roslaunch turtlebot_gazebo amcl_demo.launch " & 
+xterm  -e  " source ../../devel/setup.bash && roslaunch my_robot amcl.launch " & 
 sleep 5
-xterm  -e  " source ../../devel/setup.bash && roslaunch turtlebot_rviz_launchers view_navigation_marker.launch " &
+xterm  -e  " source ../../devel/setup.bash && roslaunch my_robot view_navigation.launch " &
 sleep 10
-xterm  -e  " source ../../devel/setup.bash && rosrun pick_objects pick_objects " &
+xterm  -e  " source ../../devel/setup.bash && rosrun add_markers add_markers " &
 sleep 5
-xterm  -e  " source ../../devel/setup.bash && rosrun add_markers add_markers "
+xterm  -e  " source ../../devel/setup.bash && rosrun pick_objects pick_objects "
